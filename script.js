de3e00a8c2284da24d3a665bef5ae5c0baef5763
faratim <104940465+faratim@users.noreply.github.com>
2022-09-06 16:04:34 -0400

    added event listener for check button

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+document
+    .querySelector('.check')
+    .addEventListener('click', function () {
+        const guess = Number(document.querySelector('.guess').value);
+
+        if (!guess) {
+            document.querySelector('.message').textContent = '⛔️ No Number!'
+        }
+    });
