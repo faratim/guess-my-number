5ad96e6621344d93d3e6bcd55cd33a2dd26b7ea0
faratim <104940465+faratim@users.noreply.github.com>
2022-09-06 18:05:32 -0400

    implemented initial game logic

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20 +1);
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
 document
     .querySelector('.check')
     .addEventListener('click', function () {
@@ -7,5 +12,27 @@ document
 
         if (!guess) {
             document.querySelector('.message').textContent = '⛔️ No Number!'
+        } else if (guess === secretNumber) {
+            document.querySelector('.message').textContent = '🎉 Correct Number!'
+        } else if (guess > secretNumber) {
+            if (score > 1) {
+                document.querySelector('.message').textContent = '📈 Too high!';
+                score--;
+                document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('.message').textContent = '💥 You lose the game!';
+                document.querySelector('.score').textContent = 0;
+            }
+            
+        } else if (guess < secretNumber) {
+            if (score >1) {
+                document.querySelector('.message').textContent = '📉 Too low!';
+                score--;
+                document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('.message').textContent = '💥 You lose the game!';
+                document.querySelector('.score').textContent = 0;
+            };
+            
         }
     });
